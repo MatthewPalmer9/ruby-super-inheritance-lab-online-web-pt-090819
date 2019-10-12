628bbd8034b48d0439c47aea189a7dc2d6641155
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-12 02:19:31 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,4 +1,9 @@
 require_relative "./student.rb"
 
 class Chatty_Student < Student
-end 
+
+  def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch THe Walking Dead last night? You didn't?! Oh man, it was so "
+  end
+end
