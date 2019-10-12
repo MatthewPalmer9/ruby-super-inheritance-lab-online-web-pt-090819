9ba63a66d8eba9faf7f4791c075af233aa13075e
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-12 02:20:12 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,6 +4,8 @@ class Chatty_Student < Student
 
   def hello
     super
-    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch THe Walking Dead last night? You didn't?! Oh man, it was so "
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch THe Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
+
+  
 end
